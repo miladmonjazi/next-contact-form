0f3f3dad1ce6059db4fac692cb8766c66afff264
Milad Monjazi <milad.monjazi@gmail.com>
2020-10-10 12:39:39 +0300

    initial commit

---
+++ pages/api/contact.js
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer"
+
+const emailPass = "241358mmB!"
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "miladtestmail@gmail.com",
+    pass: emailPass
+  }
+})
+//[1]
+
+export default async (req, res) => {
+  const { senderMail, name, content, recipientMail } = req.body
+//[2]
+
+  // Check if fields are all filled
+  if (senderMail === "" || name === "" || content === "" || recipientMail === "") {
+    res.status(403).send("")
+    return
+  }
+//[3]
+
+  const mailerRes = await mailer({ senderMail, name, text: content, recipientMail })
+  res.send(mailerRes)
+//[4]
+}
+
+const mailer = ({ senderMail, name, text, recipientMail }) => {
+  const from = name && senderMail ? `${name} <${senderMail}>` : `${name || senderMail}`
+  const message = {
+    from,
+    to: `${recipientMail}`,
+    subject: `New message from ${from}`,
+    text,
+    replyTo: from
+  }
+//[5]
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(message, (error, info) =>
+      error ? reject(error) : resolve(info)
+    )
+  })
+//[6]
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Contact from '../components/pattern/Contact'
+
+export default function Home() {
+  return (
+    <div>
+      <Contact/>
+    </div>
+  )
+}
